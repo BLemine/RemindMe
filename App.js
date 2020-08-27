f6d478ebf8460ea13e5b72f0634a651bc0f9dfad
BLemine <baillahiamine@gmail.com>
2020-08-28 00:16:36 +0100

    refactoring

--- App.js
+++ App.js
@@ -1,122 +1,125 @@
 import React, { Component } from 'react'
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View, SafeAreaView, TextInput, ScrollView
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View, SafeAreaView, TextInput, ScrollView
 } from 'react-native'
 import { Avatar, Badge, Icon, withBadge, Button, CheckBox } from 'react-native-elements'
 //import { TabNavigator } from 'react-navigation';
 
 export default class App extends Component {
-    render() {
-        return (<ScrollView>
-            <SafeAreaView style={styles.container}>
-                <View style={{ marginTop: 30, padding: 20 }}>
+  render() {
+    return (<ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={{ marginTop: 30, padding: 20 }}>
 
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
 
-                        <Icon
+            <Icon
 
-                            name='menu'
-                            type='ionicons'
-                            color='white'
-                            size={20}
+              name='menu'
+              type='ionicons'
+              color='white'
+              size={20}
 
-                        />
+            />
 
-                        <View style={{
-                            flex: 1, flexDirection: 'row', marginLeft: 140,
-                            justifyContent: 'space-between'
-                        }}>
-                            <Icon
+            <View style={{
+              flex: 1, flexDirection: 'row', marginLeft: 140,
+              justifyContent: 'space-between'
+            }}>
+              <Icon
 
-                                name='notifications-none'
-                                type='fontawesome'
-                                color='white'
-                                size={20}
+                name='notifications-none'
+                type='fontawesome'
+                color='white'
+                size={20}
 
-                            />
-                            <Icon
+              />
+              <Icon
 
-                                name='person-outline'
-                                type='fontawesome'
-                                color='white'
-                                size={20}
+                name='person-outline'
+                type='fontawesome'
+                color='white'
+                size={20}
 
-                            />
-                        </View>
+              />
+            </View>
 
-                    </View>
-                    <Text style={{ color: '#e6e6e6', fontSize: 20 }}>Hello,</Text>
-                    <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>You are welcome!</Text>
-                </View>
-                <View style={{ alignItems: 'center' }}>
-                    <TextInput placeholder='Search.....' style={styles.input} />
-                </View>
-                <View style={styles.container2}>
-                    <Text style={{ fontWeight: 'bold' }}>Today</Text>
-                    <View style={{
-                        backgroundColor: '#ff704d', width: 17, height: 3,
-                        position: 'absolute', top: 43,
+          </View>
+          <Text style={{ color: '#e6e6e6', fontSize: 20 }}>Hello,</Text>
+          <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>Welcome to your Notes Keeper</Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <TextInput placeholder='Search.....' style={styles.input} />
+        </View>
+        <View style={styles.container2}>
+          <Text style={{ fontWeight: 'bold' }}>Today</Text>
+          <View style={{
+            backgroundColor: '#ff704d', width: 17, height: 3,
+            position: 'absolute', top: 43,
+          }} />
+          <ScrollView className="cards-container">
+            <View style={{ marginTop: 20, width: 250, height: 150,borderStyle:"solid",borderWidth:1,borderRadius:12 }}>
+              <Text style={{marginLeft:10}}>Reminder 1</Text>
+            </View>
+            <View style={{ marginTop:10,width: 250, height: 150,borderStyle:"solid",borderWidth:1,borderRadius:12 }}>
+              <Text style={{marginLeft:10}}>Reminder 2</Text>
+            </View>
+          </ScrollView>
+          <View style={styles.icon}>
+            <Icon
+              name='add'
+              type='ionicons'
+              color='white'
+              size={40}
+            />
+          </View>
+        </View>
 
-                    }} />
-
-                    <View style={styles.icon}>
-                        <Icon
-
-                            name='add'
-                            type='ionicons'
-                            color='white'
-                            size={40}
-
-                        />
-                    </View>
-                </View>
-
-            </SafeAreaView >
-        </ScrollView>
-        )
-    }
+      </SafeAreaView >
+    </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
+    backgroundColor: ' rgb(92, 133, 214)',
+  },
+  container2: {
+    marginTop: '10%',
+    borderTopLeftRadius: 30,
+    backgroundColor: 'white',
+    height: 349,
+    padding: 20
+  },
+  input: {
+    //borderColor: 'none',
+    padding: 4,
+    width: '80%',
+    color: 'white',
+    height: 40,
+    borderWidth: 0,
+    borderRadius: 20,
+    backgroundColor: ' rgb(51, 102, 204)',
+    //placeholderTextColor: 'white'
 
-        backgroundColor: ' rgb(92, 133, 214)',
-    },
-    container2: {
-        marginTop: '10%',
-        borderTopLeftRadius: 30,
-        backgroundColor: 'white',
-        height: 349,
-        padding: 20
-    },
-    input: {
-        //borderColor: 'none',
-        padding: 4,
-        width: '80%',
-        color: 'white',
-        height: 40,
-        borderWidth: 0,
-        borderRadius: 20,
-        backgroundColor: ' rgb(51, 102, 204)',
-        //placeholderTextColor: 'white'
+  },
+  icon: {
 
-    },
-    icon: {
-
-        position: 'absolute',
-        width: 72, height: 72,
-        shadowColor: 'black',
-        shadowOpacity: 0.4,
-        shadowRadius: 15,
-        borderRadius: 12,
-        top: 230, left: 160,
-        backgroundColor: '#ff704d',
-        shadowOffset: { width: -1, height: 1, },
-        justifyContent: 'center'
-    }
+    position: 'absolute',
+    width: 72, height: 72,
+    shadowColor: 'black',
+    shadowOpacity: 0.4,
+    shadowRadius: 15,
+    borderRadius: 12,
+    top: 230, right: 50,
+    backgroundColor: '#ff704d',
+    shadowOffset: { width: -1, height: 1, },
+    justifyContent: 'center'
+  }
 
 })
 
